79df8879c0fb70191faa3215fe9617473c2fd4d8
Arvinderpal <awander@apcera.com>
2018-10-02 21:56:59 -0700

    Arduino: updated gettingstarted.cpp to reflect (ce,cs) pins (9,10).

--- examples/GettingStarted/GettingStarted.ino
+++ examples/GettingStarted/GettingStarted.ino
@@ -13,13 +13,13 @@
 bool radioNumber = 0;
 
 /* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
+RF24 radio(9,10);
 /**********************************************************/
 
 byte addresses[][6] = {"1Node","2Node"};
 
 // Used to control whether this node is sending or receiving
-bool role = 0;
+bool role = 1;
 
 void setup() {
   Serial.begin(115200);
